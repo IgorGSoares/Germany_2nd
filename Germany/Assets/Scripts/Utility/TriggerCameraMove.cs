8a2ea86dad0c2c3bb7698aa14d24de694eade8ca
Igor Gilberto Soares <igorgilbertosoares@gmail.com>
2024-07-21 01:17:19 -0300

    First test

--- Germany/Assets/Scripts/Utility/TriggerCameraMove.cs
+++ Germany/Assets/Scripts/Utility/TriggerCameraMove.cs
@@ -1,36 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class TriggerCameraMove : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    [HideInInspector] public float speed;
-    bool follow = false;
-
-    Rigidbody2D playerRB;
-
-    void Start()
-    {
-        playerRB = player.GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        var pos = Camera.main.transform.position;
-        if (follow)
-        {
-            Debug.Log("is following");
-            //Camera.main.transform.position = pos + player.transform.up * playerRB.velocity.y * Time.deltaTime;
-        }
-    }
+    [SerializeField] CinemachineVirtualCamera vCam;
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player") follow = true;
+        if(other.gameObject.tag == "Player") 
+        {
+            vCam.Follow = player.transform;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player") follow = false;
+        if(other.gameObject.tag == "Player")
+        {
+            vCam.Follow = null;
+        }
     }
 }
